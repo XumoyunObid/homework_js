fe29453afcf85011d31ce2bd570a2fd3213751bb
Xumoyun <obidjonov06122005@gmail.com>
2023-10-24 21:58:40 +0500

    finished

---
+++ lesson5/java.js
@@ -0,0 +1,7 @@
+let age = prompt("Yoshingiz nechada?");
+let wantedYear = prompt("Necha yoshni ko'zlab turipsiz?")
+age = Number(age);
+let year = 2023;
+console.log("Siz " + (year - age) + "-yilda tug'ilgan ekansiz");
+console.log("3 yildan so'ng siz " + (age + 3) + " yoshda bo'lasiz");
+console.log( wantedYear + " yoshga kirish uchun, " + (wantedYear - age) + " yil kerak bo'ladi")
